a254ad3a0ab5c76880071b597477269e5bf3d1a9
namngxthanh <namngx163@gmail.com>
2025-05-21 11:18:06 +0700

    Create Hero 3D model

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Hero from "./sections/Hero";
 const App = () => {
     return (
-        <main>
-            <div className="text-3xl underline">
-                <h1>Welcome to my portfolio</h1>
-            </div>
-        </main>
+        <>
+            <Hero />
+        </>
     );
 };
 
